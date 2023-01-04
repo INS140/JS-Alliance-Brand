1c894c5bf48e1512dbf5823825deab413264347b
INS140 <IsaacNSaettel@gmail.com>
2023-01-03 19:09:43 -0800

    updated

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -6,19 +6,23 @@ function addCustomerName() {
     title.textContent = customerName;
     
     // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
-
+    let h1 = document.getElementById('tm-header')
+    h1.textContent = customerName
     // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
-
+    let h2 = document.getElementById('introheader')
+    h2.textContent = customerName
 };
 
 function toggleColors() {
     // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
-
+    let ogCol = document.querySelectorAll('.tm-white-rect')
+    ogCol.forEach(e => e.classList.replace('tm-white-rect', 'tm-blue-rect'))
 };
 
 function hideElement() {
     //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
-
+    let ul = document.querySelector('.tm-social-links')
+    ul.style.visibility = 'hidden'
 };
 
  function addText() {
@@ -32,7 +36,20 @@ function hideElement() {
     //<p id="aboutpara2"> <!--todo: Add customer company motto text-->
 
     // <p id="addresspara" class="mb-3"> <!--todo: Add customer company phone number text-->
+    let ip1 = document.getElementById('intropara1')
+    ip1.textContent = 'Alliance was founded in 1931 by Albert Daniels.'
 
+    let ip2 = document.getElementById('intropara2')
+    ip2.textContent = 'The company is a leader in P.C. Manufacturing'
+
+    let ap1 = document.getElementById('aboutpara1')
+    ap1.textContent = 'We deliver the best service this side of creation'
+
+    let ap2 = document.getElementById('aboutpara2')
+    ap2.textContent = 'Our motto is satisfaction guaranteed'
+
+    let adp = document.getElementById('addresspara')
+    adp.textContent = '1-800-565-5656'
 }; 
 
 
